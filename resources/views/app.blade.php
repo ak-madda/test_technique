a6f2f4afba26142895753052a4b1f3cd9fe8f4e6
ak-madda <abdemadda@gmail.com>
2025-08-22 22:10:44 +0100

    fix template js app

---
+++ resources/views/app.blade.php
@@ -0,0 +1,18 @@
+<!doctype>html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maxium-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>                                                     
+@white()
+
+</head>
+<body>
+    <div id="app"> 
+         @yield("content") 
+
+    </div>
+   
+</body>
+</html>
