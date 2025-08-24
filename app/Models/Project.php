7f64685a65f19f733c6356325cf0fffacd85f18c
ak-madda <abdemadda@gmail.com>
2025-08-24 18:57:34 +0100

    add Project Model

---
+++ app/Models/Project.php
@@ -0,0 +1,33 @@
+<?php
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'description',
+        'user_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
+}
