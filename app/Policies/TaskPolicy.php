a637b3e56d392fdb2885b1d9394afd1381a9411b
ak-madda <abdemadda@gmail.com>
2025-08-29 12:12:25 +0100

    add Policies

---
+++ app/Policies/TaskPolicy.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Task;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class TaskPolicy
+{
+    public function view(User $user, Task $task): bool
+    {
+        return $user->id === $task->project->user_id || $user->id === $task->assigned_to;
+    }
+
+    public function update(User $user, Task $task): bool
+    {
+        return $user->id === $task->project->user_id || $user->id === $task->assigned_to;
+    }
+
+    public function delete(User $user, Task $task): bool
+    {
+        return $user->id === $task->project->user_id;
+    }
+}
