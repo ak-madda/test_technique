ad5340e5e3410756f439169ab00980d04291d4e6
ak-madda <abdemadda@gmail.com>
2025-08-30 14:10:27 +0100

    add  change to databaseseeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,14 +2,26 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
+    /**
+     * Run the database seeds.
+     */
     public function run(): void
     {
-        $this->call([
-            AdminUserSeeder::class,
+        // Clear existing users
+        User::truncate();
+        
+        // Create admin user
+        User::create([
+            'name' => 'Admin User',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('password'),
+            'email_verified_at' => now()
         ]);
     }
 }
