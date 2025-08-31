3704a1b2c824c09cca6c039facf6cd52382ebc90
ak-madda <abdemadda@gmail.com>
2025-08-31 16:01:07 +0100

    add login view

---
+++ resources/js/views/Login.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+import { useAuthStore } from '../stores/auth'
+
+const router = useRouter()
+const auth = useAuthStore()
+
+const form = ref({
+    email: '',
+    password: ''
+})
+
+const login = async () => {
+    if (await auth.login(form.value)) {
+        router.push('/dashboard')
+    }
+}
+</script>
+
+<template>
+    <form @submit.prevent="login">
+        <input v-model="form.email" type="email">
+        <input v-model="form.password" type="password">
+        <button type="submit">Login</button>
+    </form>
+</template>
