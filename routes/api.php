e5c58224af806305383520f846d206c131fa750a
ak-madda <abdemadda@gmail.com>
2025-08-31 15:28:56 +0100

    fix api routes

--- routes/api.php
+++ routes/api.php
@@ -3,25 +3,14 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\TaskController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout']);
-    
     Route::apiResource('projects', ProjectController::class);
-    
-    Route::get('/projects/{project}/tasks', [TaskController::class, 'projectTasks']);
-    Route::post('/projects/{project}/tasks', [TaskController::class, 'store']);
-    
-    Route::apiResource('tasks', TaskController::class)->except(['store']);
+    Route::apiResource('tasks', TaskController::class);
     Route::patch('/tasks/{task}/status', [TaskController::class, 'updateStatus']);
-    
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
-}
-);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
