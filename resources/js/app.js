0ff6a75075c95340b0cf1fc9ba5d02e7c7a50af3
ak-madda <abdemadda@gmail.com>
2025-08-31 15:52:26 +0100

    update app.js to use pinia

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,11 @@
 import './bootstrap';
 
 import { createApp } from 'vue';
-import TaskBoard from './components/TaskBoard.vue';
+import { createPinia } from 'pinia';
+import router from './router';
+import App from './App.vue';
 
-const app = createApp({});
-app.component('task-board', TaskBoard);
+const app = createApp(App);
+app.use(createPinia());
+app.use(router);
 app.mount('#app');
